e89cd34a5fc89656c8ac5afb2ed1422c5c92985f
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-08-28 15:43:35 +0900

    [Build] #1 프로젝트 생성 및 코드베이스 세팅(MainViewController 생성)

---
+++ BJGG/BJGG/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  BJGG
+//
+//  Created by 이재웅 on 2022/08/28.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
