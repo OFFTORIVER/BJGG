28d000d7fc36cbf4352cda2a1bcb8615bc4b7464
jeonghyeon Hwang <hijihilin@gmail.com>
2022-09-18 23:46:06 +0900

    [Feat] #5 BbajiCameraViewController 추가

---
+++ BJGG/BJGG/Spot/BbajiCameraViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SpotViewController.swift
+//  BJGG
+//
+//  Created by 황정현 on 2022/09/18.
+//
+
+import UIKit
+import SnapKit
+
+class SpotViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
