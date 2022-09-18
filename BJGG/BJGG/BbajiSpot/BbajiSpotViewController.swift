7dea4a992791322a01e9e5b0bab65effd174d524
jeonghyeon Hwang <hijihilin@gmail.com>
2022-09-19 00:28:50 +0900

    [Fix] #5 spotInfoView top inset 수정

--- BJGG/BJGG/BbajiSpot/BbajiSpotViewController.swift
+++ BJGG/BJGG/BbajiSpot/BbajiSpotViewController.swift
@@ -37,7 +37,6 @@ class BbajiSpotViewController: UIViewController {
             make.height.equalTo(viewWidth * 9 / 16)
         })
         
-        
         spotInfoView.snp.makeConstraints({ make in
             make.top.equalTo(liveCameraView.snp.bottom).inset(-16)
             make.leading.equalTo(safeArea.snp.leading).inset(16)
@@ -56,6 +55,7 @@ class BbajiSpotViewController: UIViewController {
         liveCameraView.backgroundColor = .yellow
         spotInfoView.backgroundColor = .blue
         spotWeatherInfoView.backgroundColor = .red
+        
     }
     
 }
