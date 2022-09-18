bce4dc368139d83df20f1feef2f50be34f453610
jeonghyeon Hwang <hijihilin@gmail.com>
2022-09-18 23:50:14 +0900

    [Feat] #5 뷰 컨트롤러 내부 Custom UIView 추가
    - LiveCameraView
    - SpotInfoView
    - SpotWeatherInfoView

---
+++ BJGG/BJGG/Spot/UI Component/LiveCameraView.swift
@@ -0,0 +1,20 @@
+//
+//  LiveCameraView.swift
+//  BJGG
+//
+//  Created by 황정현 on 2022/09/18.
+//
+
+import UIKit
+
+class LiveCameraView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ BJGG/BJGG/Spot/UI Component/SpotWeatherInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  SpotInfoUIView.swift
+//  BJGG
+//
+//  Created by 황정현 on 2022/09/18.
+//
+
+import UIKit
+
+class SpotWeatherInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
