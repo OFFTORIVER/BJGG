7d4ae6be9d54e37294c1ba739dfee23c26a10fc7
jeonghyeon Hwang <hijihilin@gmail.com>
2022-09-19 21:46:45 +0900

    [Feat] #5 SpotWeatherInfoView 상단 기본 Constraints 세팅

--- BJGG/BJGG/BbajiSpot/UI Component/SpotWeatherInfoView.swift
+++ BJGG/BJGG/BbajiSpot/UI Component/SpotWeatherInfoView.swift
@@ -8,16 +8,80 @@
 import UIKit
 
 class SpotWeatherInfoView: UIView {
-
+    
     required override init(frame: CGRect) {
         
         super.init(frame: frame)
+        
+        let defaultMargin: CGFloat = 20
+        let defaultMargin2: CGFloat = 12
+        
+        let weatherAddressLabel = UILabel()
+        
+        self.addSubview(weatherAddressLabel)
+        
+        weatherAddressLabel.snp.makeConstraints({ make in
+            make.leading.equalTo(self.snp.leading).inset(defaultMargin)
+            make.top.equalTo(self.snp.top).inset(defaultMargin)
+            make.height.equalTo(18)
+        })
+        
+        labelSetting(label: weatherAddressLabel, text: "땡땡구 댕댕동", size: 15, weight: .medium, alignment: .left)
+        
+        let currentWeatherIconAndLabel = UIView()
+        self.addSubview(currentWeatherIconAndLabel)
+        
+        currentWeatherIconAndLabel.snp.makeConstraints({ make in
+            make.top.equalTo(weatherAddressLabel.snp.bottom).inset(-defaultMargin2)
+            make.centerX.equalTo(self.snp.centerX)
+            make.width.equalTo(160)
+            make.height.equalTo(64)
+        })
+        
+        currentWeatherIconAndLabel.backgroundColor = .black
+        
+        let currentWeatherIcon = UIImageView()
+        let currentWeatherLabel = UILabel()
+        
+        [currentWeatherIcon, currentWeatherLabel].forEach({
+            currentWeatherIconAndLabel.addSubview($0)
+        })
+        
+        currentWeatherIcon.snp.makeConstraints({ make in
+            make.leading.equalTo(currentWeatherIconAndLabel.snp.leading)
+            make.top.equalTo(currentWeatherIconAndLabel.snp.top)
+            make.centerY.equalTo(currentWeatherIconAndLabel.snp.centerY)
+            make.width.height.equalTo(64)
+        })
+        
+        currentWeatherLabel.snp.makeConstraints({ make in
+            make.trailing.equalTo(currentWeatherIconAndLabel.snp.trailing)
+            make.centerY.equalTo(currentWeatherIcon.snp.centerY)
+            make.width.equalTo(160 - 64)
+        })
+        
+        labelSetting(label: currentWeatherLabel, text: "23°", size: 48, weight: .heavy, alignment: .center)
+        
+        currentWeatherLabel.backgroundColor = .yellow
+        
+        currentWeatherIcon.backgroundColor = .cyan
+        
+        let rainInfoLabel = UILabel()
+        self.addSubview(rainInfoLabel)
+        
+        rainInfoLabel.snp.makeConstraints({ make in
+            make.top.equalTo(currentWeatherIconAndLabel.snp.bottom).inset(-defaultMargin2)
+            make.centerX.equalTo(self.snp.centerX)
+            make.height.equalTo(18)
+        })
+        
+        labelSetting(label: rainInfoLabel, text: "오후 12시 경에 비가 올 예정이에요!", size: 15, weight: .medium, alignment: .center)
         
         let spotWeatherInfoViewDivideLine = UIView()
         self.addSubview(spotWeatherInfoViewDivideLine)
         spotWeatherInfoViewDivideLine.snp.makeConstraints({ make in
             make.leading.equalTo(self.snp.leading)
-            make.top.equalTo(self.snp.top).inset(164)
+            make.top.equalTo(rainInfoLabel.snp.bottom).inset(-defaultMargin)
             make.centerX.equalTo(self.snp.centerX)
             make.width.equalTo(self.snp.width)
             make.height.equalTo(2)
