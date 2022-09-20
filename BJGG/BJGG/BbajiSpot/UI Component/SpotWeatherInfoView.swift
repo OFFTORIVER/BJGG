d1c1ab1e9bc7d1fb5b8969f9a3207fe318019f47
jeonghyeon Hwang <hijihilin@gmail.com>
2022-09-21 02:01:54 +0900

    [Fix] #5 WeatherCollectionView 높이값 변경
    - 90 -> 100

--- BJGG/BJGG/BbajiSpot/UI Component/SpotWeatherInfoView.swift
+++ BJGG/BJGG/BbajiSpot/UI Component/SpotWeatherInfoView.swift
@@ -130,7 +130,7 @@ final class SpotWeatherInfoView: UIView {
 
 extension SpotWeatherInfoView: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 48, height: 90)
+        return CGSize(width: 48, height: 100)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
