a4a1366bf499ed5e9e6980405f6acb1c95a7673c
jeonghyeon Hwang <hijihilin@gmail.com>
2022-09-21 01:48:56 +0900

    [Style] #5 필요없는 주석 제거

--- BJGG/BJGG/BbajiSpot/UI Component/SpotLiveCameraView.swift
+++ BJGG/BJGG/BbajiSpot/UI Component/SpotLiveCameraView.swift
@@ -7,14 +7,4 @@
 
 import UIKit
 
-final class SpotLiveCameraView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
-}
+final class SpotLiveCameraView: UIView { }
