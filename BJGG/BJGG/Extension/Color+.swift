53e5a6ea1c3e0553cf7424549b7121e2b092b6b2
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-09-23 02:50:30 +0900

    [Design] #12 UIColor 클래스에 extension으로 static 상수 작성 및 주석처리

--- BJGG/BJGG/Extension/Color+.swift
+++ BJGG/BJGG/Extension/Color+.swift
@@ -5,5 +5,24 @@
 //  Created by 이재웅 on 2022/09/22.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    // Colors
+//    static let bbagaBlue = UIColor(rgb:)
+//    static let bbagaGreen = UIColor(rgb:)
+    
+    // Grayscale
+//    static let bbagaGray1 = UIColor(rgb:)
+//    static let bbagaGray2 = UIColor(rgb:)
+//    static let bbagaGray3 = UIColor(rgb:)
+//    static let bbagaGray4 = UIColor(rgb:)
+//    static let bbagaBack = UIColor(rgb:)
+    
+    // Icon Color
+//    static let bbagaSunny = UIColor(rgb:)
+//    static let bbagaRain = UIColor(rgb:)
+    
+}
+
 
