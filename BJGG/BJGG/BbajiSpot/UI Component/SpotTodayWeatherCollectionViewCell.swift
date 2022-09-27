8c8fa9df59ab177e1cd7769fafb5a13c659a578a
jeonghyeon Hwang <hijihilin@gmail.com>
2022-09-28 03:45:38 +0900

    [Fix] #17 SpotWeatherInfoView Cell 내부 Label Constraints 수정
    - temperatureLabel width 48로 설정
    - timeLabel top offset 2로 설정
    - timeLabel height 22로 설정
    - timeLabel width 58로 설정

--- BJGG/BJGG/BbajiSpot/UI Component/SpotTodayWeatherCollectionViewCell.swift
+++ BJGG/BJGG/BbajiSpot/UI Component/SpotTodayWeatherCollectionViewCell.swift
@@ -44,14 +44,15 @@ final class SpotTodayWeatherCollectionViewCell: UICollectionViewCell {
             make.leading.equalTo(currentWeatherImgView.snp.leading)
             make.centerX.equalTo(currentWeatherImgView.snp.centerX)
             make.height.equalTo(22)
+            make.width.equalTo(48)
         })
         
         self.addSubview(timeLabel)
         timeLabel.snp.makeConstraints({ make in
-            make.top.equalTo(temperatureLabel.snp.bottom).offset(CGFloat.labelOffset)
+            make.top.equalTo(temperatureLabel.snp.bottom).offset(2)
             make.centerX.equalTo(temperatureLabel.snp.centerX)
-            make.height.equalTo(18)
-            make.width.equalTo(64)
+            make.height.equalTo(22)
+            make.width.equalTo(58)
         })
     }
 }
