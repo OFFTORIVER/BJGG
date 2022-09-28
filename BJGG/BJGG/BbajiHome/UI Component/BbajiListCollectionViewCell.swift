e50b8c3cac81604ae17dc34db921f87749e22155
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-09-29 02:20:46 +0900

    [Feat] #23 BbajiCollectionViewCell Constraints 설정 및 label 설정

--- BJGG/BJGG/BbajiHome/UI Component/BbajiListCollectionViewCell.swift
+++ BJGG/BJGG/BbajiHome/UI Component/BbajiListCollectionViewCell.swift
@@ -9,6 +9,89 @@ import UIKit
 import SnapKit
 
 class BbajiListCollectionViewCell: UICollectionViewCell {
+    private lazy var backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        
+        return imageView
+    }()
+    
+    private lazy var locationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "서울 광진구 강변북로 64"
+        label.font = .bbajiFont(.body1)
+        label.textColor = .bbagaGray2
+        
+        return label
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "뚝섬유원지 선스키"
+        label.font = .bbajiFont(.heading2)
+        label.textColor = .bbagaGray4
+        
+        return label
+    }()
+    
+    private lazy var weatherImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .bbagaSunny
+        
+        return imageView
+    }()
+    
+    private lazy var tempLabel: UILabel = {
+        let label = UILabel()
+        label.text = "23℃"
+        label.font = .bbajiFont(.heading3)
+        label.textColor = .bbagaGray4
+        
+        return label
+    }()
+}
 
+private extension BbajiListCollectionViewCell {
+    func layoutConfigure() {
+        let iconWidth: CGFloat = 28.0
+        
+        [
+            backgroundImageView,
+            locationLabel,
+            nameLabel,
+            weatherImageView,
+            tempLabel
+        ].forEach { addSubview($0) }
+        
+        backgroundImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        locationLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(130.0)
+            $0.leading.equalToSuperview().inset(CGFloat.viewInset)
+            $0.trailing.equalToSuperview().inset(CGFloat.viewInset)
+        }
+        
+        nameLabel.snp.makeConstraints {
+            $0.top.equalTo(locationLabel.snp.bottom).offset(CGFloat.labelOffset)
+            $0.leading.equalTo(locationLabel.snp.leading)
+            $0.bottom.equalToSuperview().inset(CGFloat.viewInset)
+        }
+        
+        tempLabel.snp.makeConstraints {
+            $0.top.equalTo(nameLabel.snp.top)
+            $0.trailing.equalToSuperview().inset(CGFloat.viewInset)
+            $0.bottom.equalTo(nameLabel.snp.bottom)
+        }
+        
+        weatherImageView.snp.makeConstraints {
+            $0.top.equalTo(nameLabel.snp.top)
+            $0.trailing.equalTo(tempLabel.snp.leading).offset(CGFloat.labelOffset)
+            $0.width.equalTo(iconWidth)
+            $0.height.equalTo(iconWidth)
+        }
+        
+    }
 }
 
