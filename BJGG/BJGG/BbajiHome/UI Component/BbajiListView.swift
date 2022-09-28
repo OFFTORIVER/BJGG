7191eb520c8832a058abdbe58378555531f88402
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-09-29 02:24:26 +0900

    [Feat] #23 BbajiListView에 UICollectionViewDelegateFlowLayout, UICollectionViewDataSource 프로토콜 적 용

--- BJGG/BJGG/BbajiHome/UI Component/BbajiListView.swift
+++ BJGG/BJGG/BbajiHome/UI Component/BbajiListView.swift
@@ -12,6 +12,9 @@ class BbajiListView: UIView {
         let layout = UICollectionViewFlowLayout()
         let collectionView = BbajiListCollectionView(frame: .zero, collectionViewLayout: layout)
         
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
         return collectionView
     }()
     
@@ -28,4 +31,20 @@ class BbajiListView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+extension BbajiListView: UICollectionViewDelegateFlowLayout {
+    
+}
+
+extension BbajiListView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    
 }
