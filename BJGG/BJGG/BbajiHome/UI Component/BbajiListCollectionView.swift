00037c2ac063923ac9e939836da5ff7489c17c80
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-09-29 01:32:17 +0900

    [Feat] #23 BbajiListCollectionView.swift 생성, BbajiListCollectionView를 BbajiListView에 추가

---
+++ BJGG/BJGG/BbajiHome/UI Component/BbajiListCollectionView.swift
@@ -0,0 +1,19 @@
+//
+//  BbajiListCollectionView.swift
+//  BJGG
+//
+//  Created by 이재웅 on 2022/09/29.
+//
+
+import UIKit
+import SnapKit
+
+class BbajiListCollectionView: UICollectionView {
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- BJGG/BJGG/BbajiHome/UI Component/BbajiListView.swift
+++ BJGG/BJGG/BbajiHome/UI Component/BbajiListView.swift
@@ -8,5 +8,24 @@
 import UIKit
 
 class BbajiListView: UIView {
+    private lazy var bbajiListCollectionView: BbajiListCollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = BbajiListCollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        return collectionView
+    }()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(bbajiListCollectionView)
+        
+        bbajiListCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
