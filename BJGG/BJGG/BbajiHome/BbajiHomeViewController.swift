7c0c305cd10d4f91edcd8a7087b0924f9fda48c1
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-09-29 00:23:11 +0900

    [Feat] #23 BbajiHome 폴더 내에 UI Component 폴더 생성 및 BbajiTitleView, BbajiListView 이동

--- BJGG/BJGG/BbajiHome/BbajiHomeViewController.swift
+++ BJGG/BJGG/BbajiHome/BbajiHomeViewController.swift
@@ -6,10 +6,39 @@
 //
 
 import UIKit
+import SnapKit
 
 class BbajiHomeViewController: UIViewController {
+    private let bbajiTitleView = BbajiTitleView()
+    private let bbajiListView = BbajiListView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .bbagaBack
+        
+        layoutConfigure()
+    }
+}
+
+private extension BbajiHomeViewController {
+    func layoutConfigure() {
+        [
+            bbajiTitleView,
+            bbajiListView
+        ].forEach { view.addSubview($0) }
+        
+        bbajiTitleView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(51.0)
+            $0.leading.equalToSuperview().inset(CGFloat.superViewInset)
+            $0.trailing.equalToSuperview().inset(CGFloat.superViewInset)
+            $0.height.equalTo(90.0)
+        }
+        
+        bbajiListView.snp.makeConstraints {
+            $0.top.equalTo(bbajiTitleView.snp.bottom).offset(198.0)
+            $0.leading.equalToSuperview().inset(CGFloat.superViewInset)
+            $0.trailing.equalToSuperview().inset(CGFloat.superViewInset)
+            $0.bottom.equalToSuperview()
+        }
     }
 }
