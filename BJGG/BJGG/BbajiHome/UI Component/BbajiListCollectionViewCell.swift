1c5dde63b3ff83f1aabf89190a2975a2dafa91d2
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-09-29 02:26:55 +0900

    [Feat] #23 BbajiListCollecionViewCell의 private extension 설정에서 private  제거

--- BJGG/BJGG/BbajiHome/UI Component/BbajiListCollectionViewCell.swift
+++ BJGG/BJGG/BbajiHome/UI Component/BbajiListCollectionViewCell.swift
@@ -55,7 +55,7 @@ class BbajiListCollectionViewCell: UICollectionViewCell {
     }()
 }
 
-private extension BbajiListCollectionViewCell {
+extension BbajiListCollectionViewCell {
     func layoutConfigure() {
         let iconWidth: CGFloat = 28.0
         
