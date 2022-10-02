1c496d2d4a310af9da3e77ed0ab062a105995c5e
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-10-03 00:45:06 +0900

    [Feat] #45 WeatherManager 구조체의 현재시간 반환 상수 구현

--- BJGG/BJGG/Model/WeatherManager.swift
+++ BJGG/BJGG/Model/WeatherManager.swift
@@ -31,4 +31,34 @@ struct WeatherManager {
             return str.split(separator: " ").map{ String($0) }[0]
         }
     }()
+    
+    private let nowTime: String = {
+        let now = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd HHmm"
+        formatter.locale = Locale(identifier: "ko_kr")
+        formatter.timeZone = TimeZone(abbreviation: "KST")
+        
+        let str = formatter.string(from: now)
+        let time = str.split(separator: " ").map{ Int($0)! }[1]
+        
+        switch time {
+        case 200..<500:
+            return "0200"
+        case 500..<800:
+            return "0500"
+        case 800..<1100:
+            return "0800"
+        case 1100..<1400:
+            return "1100"
+        case 1400..<1700:
+            return "1400"
+        case 1700..<2000:
+            return "1700"
+        case 2000..<2300:
+            return "2000"
+        default:
+            return "2300"
+        }
+    }()
 }
