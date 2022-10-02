10f6c6c940330c30849a11c0ea3180aedf16462e
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-10-03 00:17:02 +0900

    [Feat] #45 WeatherItem 구조체의 category명 변환 함수 구현

--- BJGG/BJGG/Model/Weather.swift
+++ BJGG/BJGG/Model/Weather.swift
@@ -16,6 +16,38 @@ struct WeatherItem: Decodable {
     let fcstValue: String
     let nx: Int
     let ny: Int
+    
+    private func convertCategoryName(_ category: String) -> String {
+        switch category {
+        case "POP":
+            return "강수확률"
+        case "PTY":
+            return "강수형태"
+        case "PCP":
+            return "1시간 강수량"
+        case "REH":
+            return "습도"
+        case "SNO":
+            return "1시간 신적설"
+        case "SKY":
+            return "하늘상태"
+        case "TMP":
+            return "1시간 기온"
+        case "UUU":
+            return "풍속(동서성분)"
+        case "VVV":
+            return "풍속(남북성분)"
+        case "WAV":
+            return "파고"
+        case "VEC":
+            return "풍향"
+        case "WSD":
+            return "풍속"
+            
+        default:
+            return "예보항복 유형 판단오류"
+        }
+    }
 }
 
 struct WeatherItems: Decodable {
