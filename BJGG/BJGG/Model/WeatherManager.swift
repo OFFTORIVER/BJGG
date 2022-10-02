c39c1d40615f80d96c5e54e7aee607fdc1583190
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-10-03 00:44:20 +0900

    [Feat] #45 WeatherManager 구조체의 현재날짜 반환 상수 구현

--- BJGG/BJGG/Model/WeatherManager.swift
+++ BJGG/BJGG/Model/WeatherManager.swift
@@ -8,5 +8,27 @@
 import Foundation
 
 struct WeatherManager {
-    
+    private let today: String = {
+        let now = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd HHmm"
+        formatter.locale = Locale(identifier: "ko_kr")
+        formatter.timeZone = TimeZone(abbreviation: "KST")
+        
+        let str = formatter.string(from: now)
+        
+        let time = str.split(separator: " ").map{ Int($0)! }[1]
+        
+        switch time {
+        case 0..<200:
+            let day = str.split(separator: " ").map{ Int($0)! }[0]
+            return String(day-1)
+        case 2300...2400:
+            let day = str.split(separator: " ").map{ Int($0)! }[0]
+            return String(day-1)
+            
+        default:
+            return str.split(separator: " ").map{ String($0) }[0]
+        }
+    }()
 }
