b70f1d966293a32e5415f313093abbc9814fe292
JaewoongLee-swift <jaewoong0624@gmail.com>
2023-02-22 14:07:36 +0900

    [Refactor] #154 BbajiConstraints 구조체 및 static 변수생성

--- BJGG/BJGG/Utilities/Constraints.swift
+++ BJGG/BJGG/Utilities/Constraints.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import UIKit
+
+struct BbajiConstraints {
+    // label 간 간격 constraint
+    static let labelOffset: CGFloat = 4.0
+    
+    // icon 간 간격 constraint
+    static let iconOffset: CGFloat = 8.0
+    
+    // view 내의 component 간 간격 constraint
+    static let componentOffset: CGFloat = 12.0
+    
+    // superView와 내부 view 간 간격 constraint
+    static let superViewInset: CGFloat = 16.0
+    
+    // view와 내부 component 간 간격 constraint
+    static let viewInset: CGFloat = UIDevice.current.hasNotch ? 20.0 : 16.0
+}
