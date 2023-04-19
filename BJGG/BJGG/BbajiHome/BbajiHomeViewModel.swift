92cba26dfdd89fbe1228103d50b677f9c4e93b97
JaewoongLee-swift <jaewoong0624@gmail.com>
2023-04-19 22:11:22 +0900

    [Refactor] #198 fetchBbajiHomeWeather 구현

--- BJGG/BJGG/BbajiHome/BbajiHomeViewModel.swift
+++ BJGG/BJGG/BbajiHome/BbajiHomeViewModel.swift
@@ -8,6 +8,33 @@
 import UIKit
 import Combine
 
-class BbajiHomeViewModel {
+final class BbajiHomeViewModel: ObservableObject {
+    @Published var homeWeather: [BbajiHomeWeather] = []
     
+    private var cancellable = Set<AnyCancellable>()
+    
+    init(weatherManager: WeatherManager = WeatherManager()) {
+        fetchBbajiHomeWeather(weatherManager: weatherManager)
+    }
+    
+    func fetchBbajiHomeWeather(weatherManager: WeatherManager) {
+        let bbajiInfo = BbajiInfo()
+        let bbajiCoord = bbajiInfo.getCoordinate()
+        
+        Task {
+            do {
+                let weatherItems = try await weatherManager.requestCurrentTimeWeather(nx: bbajiCoord.x, ny: bbajiCoord.y).response.body.items
+                let weatherSet = weatherItems.requestCurrentWeatherItem()
+                self.homeWeather = weatherItems.requestWeatherDataSet(weatherSet)
+            } catch WeatherManagerError.apiError(let message) {
+                print(message)
+            } catch WeatherManagerError.networkError(let message) {
+                print(message)
+            } catch DecodingError.dataCorrupted(let description) {
+                print(description.codingPath, description.debugDescription, description.underlyingError ?? "", separator: "\n")
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
