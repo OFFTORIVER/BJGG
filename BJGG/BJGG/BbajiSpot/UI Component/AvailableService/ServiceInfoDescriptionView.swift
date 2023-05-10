a6c410f249b8339c214cbd17c011d99dbf255799
jeonghyeon Hwang <hijihilin@gmail.com>
2023-05-10 22:14:52 +0900

    [Feat] #200 Bbaji 서비스 정보 단일뷰 구현

---
+++ BJGG/BJGG/BbajiSpot/UI Component/AvailableService/ServiceInfoDescriptionView.swift
@@ -0,0 +1,62 @@
+//
+//  ServiceInfoDescriptionView.swift
+//  BJGG
+//
+//  Created by 황정현 on 2023/05/10.
+//
+
+import UIKit
+
+final class ServiceInfoDescriptionView: UIView {
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        let font = UIFont(name: UIFont.Pretendard.regular.rawValue, size: 10.0) ?? UIFont()
+        label.textColor = .bbagaGray2
+        label.configureLabelStyle(font: font, alignment: .center)
+        return label
+    }()
+    
+    private let descriptionImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+//        backgroundColor = .yellow
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        configureLayout()
+    }
+    
+    private func configureLayout() {
+        [descriptionImageView, descriptionLabel].forEach({
+            self.addSubview($0)
+        })
+        
+        descriptionImageView.snp.makeConstraints({ make in
+            make.top.equalTo(self.snp.top)
+            make.centerX.equalTo(self.snp.centerX)
+            make.width.equalTo(32)
+            make.height.equalTo(32)
+        })
+        
+        descriptionLabel.snp.makeConstraints({ make in
+            make.top.equalTo(descriptionImageView.snp.bottom).offset(6)
+            make.centerX.equalTo(descriptionImageView.snp.centerX)
+            make.width.equalTo(38)
+            make.height.equalTo(12)
+        })
+    }
+    
+    func configureComponent(serviceInfo: BbajiServiceInfo) {
+        print(serviceInfo.rawValue)
+        descriptionImageView.image = UIImage(named: serviceInfo.rawValue)
+        descriptionLabel.text = serviceInfo.serviceInfoName()
+    }
+
+}
