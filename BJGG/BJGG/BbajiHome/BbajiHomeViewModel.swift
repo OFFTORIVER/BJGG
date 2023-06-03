4e858a04dbc1ca253d39e06c879ee2d367ba3bd3
JaewoongLee-swift <jaewoong0624@gmail.com>
2023-06-04 02:11:06 +0900

    [Refactor] #198 BbajiHomeViewModel의 fetchweatherNows 메소드 내에서 weatherManager 에러처리 추가

--- BJGG/BJGG/BbajiHome/BbajiHomeViewModel.swift
+++ BJGG/BJGG/BbajiHome/BbajiHomeViewModel.swift
@@ -12,7 +12,7 @@ final class BbajiHomeViewModel: ObservableObject {
     
     private var bbajiInfo = CurrentValueSubject<BbajiInfo, Never>(BbajiInfo())
     private var cancellable = Set<AnyCancellable>()
-    private var weatherManager: WeatherManager!
+    private var weatherManager: WeatherManager
     
     init(weatherManager: WeatherManager = WeatherManager()) {
         self.weatherManager = weatherManager
@@ -29,21 +29,32 @@ final class BbajiHomeViewModel: ObservableObject {
             
             Task { [weak self] in
                 guard let self = self else { return }
-                let weatherItems = try await self.weatherManager.requestCurrentTimeWeather(nx: bbajiCoord.x, ny: bbajiCoord.y).response.body.items
-                let weatherItemArray = weatherItems.requestCurrentWeatherItem()
-                guard let weatherData = weatherItems.requestWeatherDataSet(weatherItemArray).first else {
-                    print("[BbajiHomeViewModel] 날씨 데이터 오류")
-                    return
+                
+                do {
+                    let weatherItems = try await self.weatherManager.requestCurrentTimeWeather(nx: bbajiCoord.x, ny: bbajiCoord.y).response.body.items
+                    let weatherItemArray = weatherItems.requestCurrentWeatherItem()
+                    guard let weatherData = weatherItems.requestWeatherDataSet(weatherItemArray).first else {
+                        print("[BbajiHomeViewModel] 날씨 데이터 오류")
+                        return
+                    }
+                    
+                    let weatherNow = BbajiWeatherNow(
+                        locationName: info.getCompactAddress(),
+                        name: info.getName(),
+                        backgroundImageName: info.getThumbnailImgName(),
+                        iconName: weatherData.iconName,
+                        temp: weatherData.temp)
+                    
+                    self.weatherNows = [weatherNow]
+                } catch WeatherManagerError.apiError(let message) {
+                    print(message)
+                } catch WeatherManagerError.networkError(let message) {
+                    print(message)
+                } catch DecodingError.dataCorrupted(let description) {
+                    print(description.codingPath, description.debugDescription, description.underlyingError ?? "", separator: "\n")
+                } catch {
+                    print(error.localizedDescription)
                 }
-                
-                let weatherNow = BbajiWeatherNow(
-                    locationName: info.getCompactAddress(),
-                    name: info.getName(),
-                    backgroundImageName: info.getThumbnailImgName(),
-                    iconName: weatherData.iconName,
-                    temp: weatherData.temp)
-                
-                self.weatherNows = [weatherNow]
             }
         }
         .store(in: &cancellable)
