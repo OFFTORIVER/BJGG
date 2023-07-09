80b6419341d43e1696ce41621d41dbbaebe749ca
jeonghyeon Hwang <hijihilin@gmail.com>
2023-07-09 22:21:57 +0900

    [Feat] #209 sceneDisconnect 시 monitoring stop 메소드 추가

--- BJGG/BJGG/SceneDelegate.swift
+++ BJGG/BJGG/SceneDelegate.swift
@@ -28,14 +28,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             self.window?.rootViewController = navigationController
         }
         
-        NetworkManager.shared.refactorStartMonitoring(window: window)
+        NetworkManager.shared.startMonitoring(window: window)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        NetworkManager.shared.stopMonitoring()
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
