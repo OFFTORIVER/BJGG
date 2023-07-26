4ea450a530796fa2aaa44f8d00415fb85d04c396
JaewoongLee-swift <jaewoong0624@gmail.com>
2023-07-26 21:43:08 +0900

    [Feat] BJGGTests 타겟 및 ForecastManagerTests 생성

---
+++ BJGG/BJGGTests/ForecastManagerTests.swift
@@ -0,0 +1,20 @@
+//
+//  ForecastManagerTests.swift
+//  BJGGTests
+//
+//  Created by Jaewoong Lee on 2023/07/26.
+//
+
+import XCTest
+
+final class ForecastManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+}
