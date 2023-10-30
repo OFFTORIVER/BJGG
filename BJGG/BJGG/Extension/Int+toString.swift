daca276caa3dee71b41455d81ae7e717dbd1e6bb
JaewoongLee-swift <jaewoong0624@gmail.com>
2023-10-30 23:12:38 +0900

    [Feat] #208 Int를 String으로 변환하는 extension 메소드 toStirng() 구현

---
+++ BJGG/BJGG/Extension/Int+toString.swift
@@ -0,0 +1,14 @@
+//
+//  Int+toString.swift
+//  BJGG
+//
+//  Created by Jaewoong Lee on 2023/10/30.
+//
+
+import Foundation
+
+extension Int {
+    func toString() -> String {
+        String(self)
+    }
+}
